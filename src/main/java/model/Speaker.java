6be815191f1d1821ac5ea4829396f4ec506248eb
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-10-01 20:20:31 -0300

    fix(Speaker.java): added setters

--- src/main/java/model/Speaker.java
+++ src/main/java/model/Speaker.java
@@ -25,4 +25,7 @@ public class Speaker{
     public long getId() {return this.id;}
     public String getNome() {return this.nome;}
     public String getCpf() {return this.cpf;}
+
+    public void setNome(String nome){this.nome = nome;}
+    public void setCpf(String cpf){this.cpf = cpf;}
 }
