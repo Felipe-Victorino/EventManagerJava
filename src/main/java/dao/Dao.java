2afe14cc7a01d580d79a74d2c5e8a4f411112dce
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-10-01 21:10:48 -0300

    feat(Dao.java): Refactored and implemented an abstraction root for the data acess objects

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -1,13 +1,53 @@
 package dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
-public interface Dao<T> {
+public abstract class Dao<T> implements InterfaceDao<T>{
 
-    void insert(T t);
-    void delete(T t);
-    void update(T t);
-    T searchBy(long id);
-    List<T> searchAll();
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersistenciaPU");
 
+	Class<T> cls;
+
+	public Dao(Class<T> cls){
+		this.cls = cls;
+	}
+
+	public void insert(T t){
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(t);
+		em.getTransaction().commit();
+		em.close();
+	};
+	public void delete(T t){
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.remove(t);
+		em.getTransaction().commit();
+		em.close();
+	};
+	public void update(T t){
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.merge(t);
+		em.getTransaction().commit();
+		em.close();
+	};
+	public T searchBy(long id){
+		EntityManager em = emf.createEntityManager();
+		try{
+			return em.find(cls, id);
+		} finally{
+			em.close();
+		}
+	};
+	public List<T> searchAll(){
+		EntityManager em = emf.createEntityManager();
+		Query query = em.createQuery("from " + cls.getSimpleName());
+		return (List<T>) query.getResultList();
+	};
 }
